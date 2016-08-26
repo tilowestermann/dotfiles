b50ad2754b615c290b28c3a96ca5ed899165ed88
Tilo Westermann <mail@tilowestermann.eu>
2016-08-26 15:36:18 +0200

    Added aliases for cd

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,8 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias numFiles='echo $(ls -1 | wc -l)'
+
+# cd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
